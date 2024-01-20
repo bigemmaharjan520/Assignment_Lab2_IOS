5e3d8db64109551f0e5c02f974a9a2c630b982e6
Bigem Maharjan <bigemmaharjan520@gmail.com>
2024-01-20 14:57:31 -0800

    Conestoga Logo, launch screen, main, project code

--- Assignment_Lab2/ViewController.swift
+++ Assignment_Lab2/ViewController.swift
@@ -8,7 +8,41 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var showNumber = 0;
+    
+    @IBOutlet var Number: UILabel!
+    
+    
+    
+    @IBAction func Incline(_ sender: Any) {
+        showNumber += 1
+        Number.text = String(showNumber)
+    }
+    
+    @IBAction func Decline(_ sender: Any) {
+        if(showNumber == 0){
+            showNumber = 0
+            Number.text = String(showNumber)
+        }else{
+            showNumber -= 1
+            Number.text = String(showNumber)
+        }
+    }
+    
+    
+    
+    @IBAction func Reset(_ sender: Any) {
+        showNumber = 0
+        Number.text = String(showNumber)
+    }
+    
+    
+    @IBAction func AddTwoStep(_ sender: Any) {
+        showNumber += 2
+        Number.text = String(showNumber)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
