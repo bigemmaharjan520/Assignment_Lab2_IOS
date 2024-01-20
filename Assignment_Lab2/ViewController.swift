6a028617c20cd46369b2238f1e70e3c5477f2393
Bigem Maharjan <bigemmaharjan520@gmail.com>
2024-01-20 06:14:08 -0800

    Initial Commit

---
+++ Assignment_Lab2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Assignment_Lab2
+//
+//  Created by user240741 on 1/20/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
